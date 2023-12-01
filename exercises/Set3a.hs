9bc7b4110bc2d6149b23563231a68bcba1d66846
Dong <kd100150@gmail.com>
2023-12-01 17:00:51 +0900

    Solve Set3a.hs Ex.12

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -246,7 +246,7 @@ sumRights xs = sum $ map (either (const 0) id) xs
 --   multiCompose [(3*), (2^), (+1)] 0 ==> 6
 --   multiCompose [(+1), (2^), (3*)] 0 ==> 2
 
-multiCompose fs = todo
+multiCompose fs = foldr (.) id fs
 
 ------------------------------------------------------------------------------
 -- Ex 13: let's consider another way to compose multiple functions. Given
